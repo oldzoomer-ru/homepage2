8691daef10468b6dd5945296f01d194bdbe23627
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-09-15 18:24:42 +0300

    Refactor: Remove ThemeManager and related theme/responsive logic

--- src/main/java/ru/oldzoomer/application/base/ui/MainView.java
+++ src/main/java/ru/oldzoomer/application/base/ui/MainView.java
@@ -4,7 +4,6 @@ import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.router.RouterLayout;
 
 import ru.oldzoomer.application.base.ui.styles.StyleUtils;
-import ru.oldzoomer.application.base.ui.styles.ThemeManager;
 
 /**
  * Главная страница - корневой view приложения
@@ -15,8 +14,5 @@ public final class MainView extends Main implements RouterLayout {
         // Применяем стили основного контейнера
         StyleUtils.applyBodyStyle(this);
         StyleUtils.applyBaseStyle(this);
-        
-        // Инициализируем темы и responsive стили
-        ThemeManager.initializeRootStyles(this);
     }
 }
--- src/main/java/ru/oldzoomer/application/base/ui/components/HeaderComponent.java
+++ src/main/java/ru/oldzoomer/application/base/ui/components/HeaderComponent.java
@@ -4,7 +4,6 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 
 import ru.oldzoomer.application.base.ui.styles.StyleUtils;
-import ru.oldzoomer.application.base.ui.styles.ThemeManager;
 
 /**
  * Компонент заголовка страницы
@@ -19,7 +18,6 @@ public class HeaderComponent extends Div {
         H1 title = new H1("oldzoomer");
         StyleUtils.applyTitle1Style(title);
         StyleUtils.applyBaseStyle(title);
-        ThemeManager.applyResponsiveTitleStyles(title, "title-1");
         title.getElement().setAttribute("aria-label", "Персональная страница oldzoomer");
         
         // Подзаголовок
@@ -27,7 +25,6 @@ public class HeaderComponent extends Div {
         subtitle.setText("Фидошник, Java-разработчик");
         StyleUtils.applyTitle2Style(subtitle);
         StyleUtils.applyBaseStyle(subtitle);
-        ThemeManager.applyResponsiveTitleStyles(subtitle, "title-2");
         subtitle.getElement().setAttribute("aria-label", "Род деятельности: Фидошник, Java-разработчик");
         
         // Добавляем компоненты
